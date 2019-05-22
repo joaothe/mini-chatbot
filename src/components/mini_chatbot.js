47cf86d2c0cad821e5abefeddc75ed013386bedd
Joao <joaoded@hotmail.com>
2019-05-22 16:51:45 -0300

    Adding example

--- src/components/mini_chatbot.js
+++ src/components/mini_chatbot.js
@@ -6,13 +6,52 @@ import Bot from 'react-native-chatbot';
 //Instructions that chatbot will follow
 const steps = [
   {
-    id: '0',
-    message: 'Welcome to react chatbot!',
-    trigger: '1',
+    id: '1',
+    message: 'Hello, my name is joao, what is your name?',
+    trigger: '2',
   },
   {
-    id: '1',
-    message: 'Bye!',
+    id: '2',
+    user: true,
+    trigger: 'ola',
+  },
+  {
+    id: 'ola',
+    message: 'Hello {previousValue}',
+    trigger: '3',
+  },
+  {
+    id: '3',
+    message: 'What do you want to know?',
+    trigger: '4',
+  },
+  {
+    id: '4',
+    options: [
+      { value: 1, label: 'How old are you?', trigger: '5' },
+      { value: 2, label: 'What do you study?', trigger: '6' },
+      { value: 3, label: 'Do you work with?', trigger: '7' },
+      { value: 4, label: 'Nice to meet you', trigger: '8' },
+    ],
+  },
+  {
+    id: '5',
+    message: 'I am 30 years old!',
+    trigger: '3',
+  },
+  {
+    id: '6',
+    message: 'Computer Science!',
+    trigger: '3',
+  },
+  {
+    id: '7',
+    message: 'Software development',
+    trigger: '3',
+  },
+  {
+    id: '8',
+    message: 'Nice to meet you too',
     end: true,
   },
 ];
